8862ab0a930fe03a12f160005bffcffe512ed670
Mert Yiğit <meert.ygtt@gmail.com>
2024-06-11 15:30:36 +0300

    Game UI Design

---
+++ ZombieAttack/Assets/GameFolders/Scripts/Concretes/Movements/Gravity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieAttack.Movements
+{
+    public class Gravity : MonoBehaviour
+    {
+        [SerializeField] float _graviy = -9.81f;
+        private CharacterController _characterController;
+        private Vector3 _velocity;
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            if (_characterController.isGrounded) _velocity.y = 0f;
+
+            _velocity.y += _graviy * Time.deltaTime;
+
+            _characterController.Move(_velocity * Time.deltaTime);
+        }
+    }
+}
